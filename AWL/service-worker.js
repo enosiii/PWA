a1c0fe7d81561ed398be36f0411e050d8f41323b
enosiii <121915517+enosiii@users.noreply.github.com>
2025-01-31 22:07:44 +0800

    Update service-worker.js

--- AWL/service-worker.js
+++ AWL/service-worker.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = 'my-pwa-cache-v1';
+const CACHE_NAME = 'pwa-cache-v1';
 const urlsToCache = [
-  'https://script.google.com/macros/s/AKfycbwpdAJmvep38oEly6CRrLjE7tgO-nbwGyL3yG5HVUjmbUtr7IiwOBTe02n_SJaFVUig/exec',
+  'https://enosiii.github.io/Anime-Watch-List/',
   'https://enosiii.github.io/PWA/AWL/PC-icon-192x192.png',
   'https://enosiii.github.io/PWA/AWL/PC-icon-512x512.png'
 ];
 
+// Install event
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -12,9 +13,25 @@ self.addEventListener('install', (event) => {
   );
 });
 
+// Fetch event
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => response || fetch(event.request))
+  );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
   );
 });
