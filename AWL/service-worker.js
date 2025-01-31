c34385ad1434e8eee7ba8ed77c3aaf22b9a15ccd
enosiii <121915517+enosiii@users.noreply.github.com>
2025-01-31 20:33:10 +0800

    Update service-worker.js

--- AWL/service-worker.js
+++ AWL/service-worker.js
@@ -1,8 +1,8 @@
 const CACHE_NAME = 'my-pwa-cache-v1';
 const urlsToCache = [
   'https://script.google.com/macros/s/AKfycbwpdAJmvep38oEly6CRrLjE7tgO-nbwGyL3yG5HVUjmbUtr7IiwOBTe02n_SJaFVUig/exec',
-  'https://username.github.io/PWA/AWL/PC-icon-192x192.png',
-  'https://username.github.io/PWA/AWL/PC-icon-512x512.png'
+  'https://enosiii.github.io/PWA/AWL/PC-icon-192x192.png',
+  'https://enosiii.github.io/PWA/AWL/PC-icon-512x512.png'
 ];
 
 self.addEventListener('install', (event) => {
