3398fa37571a844785f6bd990001657cf205970d
enosiii <121915517+enosiii@users.noreply.github.com>
2025-02-01 22:21:30 +0800

    Create service-worker.js

---
+++ YTP/service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'pwa-ytp-cache-v1';
+const urlsToCache = [
+  'https://enosiii.github.io/YT-Playlist/',
+  'https://enosiii.github.io/PWA/AWL/yt1-icon-192x192.png',
+  'https://enosiii.github.io/PWA/AWL/yt1-icon-512x512.png'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
